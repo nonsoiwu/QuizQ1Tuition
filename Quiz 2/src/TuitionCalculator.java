888a49e065f37796158bb64e49ecf7ebfd227532
User <User@MyToshiba>
2016-09-26 01:03:10 -0400

    Initial Commit

---
+++ Quiz 2/src/TuitionCalculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TuitionCalculator {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("Enter your intial tuition:");
+		Scanner tuition = new Scanner(System.in);
+		float Tuition = tuition.nextFloat();
+		
+		System.out.println("Enter the percentage increase:");
+		Scanner percentage = new Scanner(System.in);
+		float Percentage = percentage.nextFloat();
+		
+		float Debt = Tuition;
+		
+		for(int year=1; year<4; year++){
+			float AddDebt = Tuition * (1+Percentage);
+			Debt = Debt + AddDebt;
+			Tuition = AddDebt;
+		}
+		System.out.println(String.format("The cost to complete your degree is: %.2f",Debt));
+	}
+}
